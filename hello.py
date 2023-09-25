ba11f130453e9c47e23297c7ddadcca3c157af20
Ultare1717 <67229334+Ultare1717@users.noreply.github.com>
2023-09-24 20:34:10 -0400

    Added Form with Name which changes on submission and has flash and error validator for name field

--- hello.py
+++ hello.py
@@ -1,16 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField, HiddenField
+from wtforms.validators import DataRequired, InputRequired, ValidationError, Email
+
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '*BlahBlahBlah*'
 moment = Moment(app)
-
 bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data.split()[0]:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data.split()[0]
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 @app.route('/user/<name>')
 def user(name):
